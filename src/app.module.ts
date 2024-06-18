1455963e6c811d42ed4617aed82b0fd19f232a7d
Joao Oliveira <joao.neto@grover.com>
2024-06-18 15:30:20 -0300

    chore: generate supergraph schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,68 @@
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import {
   utilities as nestWinstonModuleUtilities,
   WinstonModule,
 } from 'nest-winston';
 import * as winston from 'winston';
+// TODO: install jsonwebtoken
+//import { verify, decode } from 'jsonwebtoken';
 import { HealthModule } from './health/health.module';
+
+// https://github.com/tkssharma/nestjs-with-apollo-federation-gateway/blob/main/packages/getway-service/src/app.module.ts
+// const getToken = (authToken: string): string => {
+//   console.log(authToken);
+//   const match = authToken.match(/^Bearer (.*)$/);
+//   if (!match || match.length < 2) {
+//     throw new HttpException(
+//       { message: INVALID_BEARER_TOKEN },
+//       HttpStatus.UNAUTHORIZED,
+//     );
+//   }
+//   console.log(match[1]);
+//   return match[1];
+// };
+
+// const decodeToken = (tokenString: string) => {
+//   // TODO: Add secret key
+//   const decoded = decode(tokenString, process.env.SECRET_KEY);
+//   if (!decoded) {
+//     throw new HttpException(
+//       { message: INVALID_AUTH_TOKEN },
+//       HttpStatus.UNAUTHORIZED,
+//     );
+//   }
+//   return decoded;
+// };
+
+// const handleAuth = ({ req }) => {
+//   try {
+//     if (req.headers.authorization) {
+//       const token = getToken(req.headers.authorization);
+//       const verified = verify(token, process.env.SECRET_KEY);
+//       if (!verified) {
+//         throw new HttpException(
+//           { message: INVALID_AUTH_TOKEN },
+//           HttpStatus.UNAUTHORIZED,
+//         );
+//       }
+//       const decoded: any = decodeToken(token);
+//       return {
+//         userId: decoded.userId,
+//         permissions: decoded.permissions,
+//         authorization: `${req.headers.authorization}`,
+//       };
+//     }
+//   } catch (err) {
+//     throw new UnauthorizedException(
+//       'User unauthorized with invalid authorization Headers',
+//     );
+//   }
+// };
 
 @Module({
   imports: [
@@ -24,7 +78,27 @@ import { HealthModule } from './health/health.module';
           .get('SUBGRAPH')
           .split('|');
         return {
+          server: {
+            // TODO: uncoment
+            // context: handleAuth,
+            playground: false,
+            plugins: [ApolloServerPluginLandingPageLocalDefault()],
+          },
           gateway: {
+            buildService: ({ url }) => {
+              return new RemoteGraphQLDataSource({
+                url,
+                willSendRequest({ request, context }: any) {
+                  request.http.headers.set('userId', context.userId);
+                  // for now pass authorization also
+                  request.http.headers.set(
+                    'authorization',
+                    context.authorization,
+                  );
+                  request.http.headers.set('permissions', context.permissions);
+                },
+              });
+            },
             supergraphSdl: new IntrospectAndCompose({
               subgraphs: [{ name: subGraphName, url: subGraphHost }],
             }),
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3002'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH'],
+    credentials: true,
+  });
   const configService = app.get(ConfigService);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   const port = configService.get('SERVICE_PORT');
